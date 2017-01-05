e7ae981b46cdc5e2316443f95b3ef7c0aa1884a4
Ethan Selzer <ethan.selzer@hautelook.com>
2017-01-04 17:40:41 -0800

    add code display to hamburger example

--- example/src/pages/Hamburger.js
+++ example/src/pages/Hamburger.js
@@ -10,10 +10,52 @@ import Header from '../components/Header';
 import Hamburger from '../components/Hamburger';
 
 import qr from '../../images/hamburger-qr.png';
-import hamburgerCode from '../../images/hamburger-code.png';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../styles/app.css';
+
+import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/dist/light';
+import js from 'highlight.js/lib/languages/javascript';
+import solarized from 'react-syntax-highlighter/dist/styles/solarized-light';
+
+registerLanguage('javascript', js);
+
+const codeString = `
+<ReactLazyTree {...{
+    data: data.nodes,
+
+    mapInitialActiveNode: (node) => {
+        return node.label === 'Dresses';
+    },
+
+    mapListItemClassName: ({ depth, isLeafNode, isOnActivePath }) => {
+        const icon = isLeafNode
+            ? 'hamburger__node--leaf'
+            : \`hamburger__node--\${isOnActivePath ? 'expanded' : 'contracted'}\`;
+        const leaf = \`\${isOnActivePath && isLeafNode ? 'hamburger__node--active' : ''}\`;
+        const nodeDepth = \`hamburger__node--depth-\${depth}\`;
+
+        return \`hamburger__node \${nodeDepth} \${leaf} \${icon}\`;
+    },
+
+    mapListClassName: ({ depth }) => {
+        const hamburgerDepth = \`hamburger--depth-\${depth}\`;
+
+        return \`hamburger \${hamburgerDepth}\`;
+    },
+
+    mapNodeContent: ({ depth, node }) => {
+        const base = 'hamburger__label';
+        const modifier = \`hamburger__label--depth-\${depth}\`;
+        const deselect = 'hamburger--deselect';
+        const className = \`\${base} \${modifier} \${deselect}\`;
+
+        return (
+            <Label className={className} text={node.label} />
+        );
+    }
+}} />`;
+
 
 
 class  HamburgerPage extends Component {
@@ -28,10 +70,10 @@ class  HamburgerPage extends Component {
                 </Jumbotron>
                 <Grid>
                     <Row>
-                        <Col sm={6} md={6} lg={6}>
+                        <Col sm={6} md={4} lg={4}>
                             <Hamburger/>
                         </Col>
-                        <Col sm={6} md={6} lg={6}>
+                        <Col sm={6} md={8} lg={8}>
                             <a
                                 className="hamburger__qr-container"
                                 href="#/Hamburger/Touch"
@@ -39,7 +81,10 @@ class  HamburgerPage extends Component {
                                 <img className="hamburger__qr" src={qr} alt="QR Code" />
                                 <div className="hamburger__qr-caption">Mobile Specific View</div>
                             </a>
-                            <img src={hamburgerCode} width="500" alt="Source code for hamburger menu example" />
+
+                            <SyntaxHighlighter language='javascript' style={solarized}>
+                                {codeString}
+                            </SyntaxHighlighter>
                         </Col>
                     </Row>
                 </Grid>
